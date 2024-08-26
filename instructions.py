92cdc210cc9bd80eb17e2fac4b8f98d0b01c948e
Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
2024-08-26 18:55:31 +0100

    Update instructions.py

--- instructions.py
+++ instructions.py
@@ -8,13 +8,14 @@
 # Modifier 135 - 254: TBD
 # Modifier 255: Common Functions (Default Modifier)
 
+import math
+
 def constant(c):
     def inner(stack):
         return stack.append(c)
     return inner
 
 instructions = {132: {
-    0: constant("")
     1: constant("hello world")
     2: constant("Hello World")
     3: constant("Hello world")
@@ -23,6 +24,19 @@ instructions = {132: {
     6: constant("HELLO WORLD!")
     7: constant("Hello, World")
     8: constant("Hello world!")
+    9: constant("")
+    10: constant(1)
+    11: constant(2)
+    12: constant(3)
+    13: constant(4)
+    14: constant(5)
+    15: constant(6)
+    16: constant(7)
+    17: constant(8)
+    18: constant(9)
+    19: constant(0)
+    20: constant(10)
+    21: constant(26)
 }}
 
 def instruction(dynins, insmod):
@@ -38,3 +52,39 @@ def instruction(dynins, insmod):
             return func(*a, **k)
         return inner
     return decor
+
+@instruction(1, 128)
+def p10(stack):
+    stack.append(10 ** stack.pop())
+
+@instruction(2, 128)
+def pe(stack):
+    stack.append(math.e ** stack.pop())
+
+@instruction(3, 128)
+def a(stack):
+    stack.append(stack.pop() + stack.pop())
+
+@instruction(4, 128)
+def s(stack):
+    a, b = stack.pop(), stack.pop()
+    stack.append(b - a)
+
+@instruction(5, 128)
+def m(stack):
+    stack.append(stack.pop() * stack.pop())
+
+@instructions(6, 128)
+def d(stack):
+    a, b = stack.pop(), stack.pop()
+    stack.append(b / a)
+
+@instructions(7, 128)
+def p(stack):
+    a, b = stack.pop(), stack.pop()
+    stack.append(b ** a)
+
+@instructions(8, 128)
+def sn(stack):
+    a, b = stack.pop(), stack.pop()
+    stack.append(b * 10 ** a)
